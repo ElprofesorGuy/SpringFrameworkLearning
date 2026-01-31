1469315bcb1f1e6b5d9503bd6a80c362e4fd2c53
ElprofesorGuy <guyeinstein15@gmail.com>
2026-01-31 12:50:08 +0100

    Add ressources for project

---
+++ src/main/java/elprofesor/spring/springsection5/SpringSection5Application.java
@@ -0,0 +1,13 @@
+package elprofesor.spring.springsection5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSection5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSection5Application.class, args);
+    }
+
+}
