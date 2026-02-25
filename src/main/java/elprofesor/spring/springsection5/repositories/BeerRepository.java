278da5c8deb65568a7e4d3f914fcd79769288388
ElprofesorGuy <guyeinstein15@gmail.com>
2026-02-25 17:17:50 +0100

    add method in the repository for get list of beers by name

--- src/main/java/elprofesor/spring/springsection5/repositories/BeerRepository.java
+++ src/main/java/elprofesor/spring/springsection5/repositories/BeerRepository.java
@@ -3,8 +3,10 @@ package elprofesor.spring.springsection5.repositories;
 import elprofesor.spring.springsection5.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID>{
-    UUID id(UUID id);
+    //UUID id(UUID id);
+    List<Beer> findByBeerNameIsLikeIgnoreCase(String beerName);
 }
