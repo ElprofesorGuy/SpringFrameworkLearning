4f1eb88fcdb6b066fd979d5872191f8db2653490
ElprofesorGuy <guyeinstein15@gmail.com>
2026-02-25 17:12:31 +0100

    edit tests for bootstrap load data

--- src/test/java/elprofesor/spring/springsection5/bootstrap/BootstrapDataTest.java
+++ src/test/java/elprofesor/spring/springsection5/bootstrap/BootstrapDataTest.java
@@ -2,15 +2,19 @@ package elprofesor.spring.springsection5.bootstrap;
 
 import elprofesor.spring.springsection5.repositories.BeerRepository;
 import elprofesor.spring.springsection5.repositories.CustomerRepository;
+import elprofesor.spring.springsection5.services.BeerCsvService;
+import elprofesor.spring.springsection5.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
     @Autowired
     BeerRepository beerRepository;
@@ -18,11 +22,14 @@ class BootstrapDataTest {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    BeerCsvService beerCsvService;
+
     BootstrapData bootstrapData;
 
     @BeforeEach
     void setUp(){
-        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, beerCsvService);
     }
 
     @Test
