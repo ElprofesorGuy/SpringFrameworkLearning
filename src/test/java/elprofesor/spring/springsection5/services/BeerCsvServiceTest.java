bdfb7167ba76e5b712b70adcce224f6cc322d70e
ElprofesorGuy <guyeinstein15@gmail.com>
2026-02-25 16:44:37 +0100

    add test for our packages : bootstrap, services, controller

---
+++ src/test/java/elprofesor/spring/springsection5/services/BeerCsvServiceTest.java
@@ -0,0 +1,26 @@
+package elprofesor.spring.springsection5.services;
+
+import elprofesor.spring.springsection5.model.BeerCSVRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeerCsvServiceTest {
+
+    BeerCsvService beerCsvService = new BeerCsvServiceImpl();
+
+    @Test
+    void convertCSV() throws FileNotFoundException {
+        File file = ResourceUtils.getFile("classpath:csvdata/beers.csv");
+        List<BeerCSVRecord> recs = beerCsvService.convertCsv(file);
+        System.out.println(recs.size());
+        assertThat(recs.size()).isGreaterThan(0);
+    }
+
+}
